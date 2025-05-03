f8ad11276994c079ceb8a58ed17eaa12541769a8
Nick De la Cruz <48021240+nickdelacruz@users.noreply.github.com>
2025-05-03 10:01:26 +0800

    Fixed LSP plugin issues

--- nvim/lua/plugins/colorScheme.lua
+++ nvim/lua/plugins/colorScheme.lua
@@ -1,12 +1,18 @@
 return {
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", enabled = false },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
