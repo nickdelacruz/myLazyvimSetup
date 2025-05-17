bb84ca0c0b8fa46fd288ca3d03379ad8c7c1ce0e
Nick De la Cruz <48021240+nickdelacruz@users.noreply.github.com>
2025-05-17 10:31:38 +0800

    added toggleTerm

---
+++ nvim/lua/plugins/toggleTerm.lua
@@ -0,0 +1,5 @@
+return {
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+  --you or
+  --{'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}},
+}
